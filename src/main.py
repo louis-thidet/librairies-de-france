842744bd8e41f496e6f0b933127a70f4f44694fe
louis-thidet <louis.thidet@protonmail.com>
2024-04-10 21:19:16 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -63,4 +63,3 @@ app.layout = html.Div(children=[
 port = int(os.environ.get("PORT", 10000))
 if __name__ == '__main__':
     app.run_server(debug=False, host='0.0.0.0', port=port)
-    print("run")
